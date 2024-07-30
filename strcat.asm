abd01fab3802eaecfcdc22592bf12c54a1d7d720
João Guilherme Miranda <114231096+JGMirand4@users.noreply.github.com>
2024-07-30 16:12:28 -0300

    Update strcat.asm - Corrigindo erro de impressão
    Correção do erro de impressão
    
    strcat.asm - OK

--- strcat.asm
+++ strcat.asm
@@ -1,18 +1,18 @@
 # Arquivo:	strcat.asm
-# Propósito: 	Concatenação de duas Strings 
+# PropÃ³sito: 	ConcatenaÃ§Ã£o de duas Strings 
 # Autores: 	Higor Matheus da Costa Cordeiro, 
-#		Cauã Ferraz Bittencourt,
-#		João Guilherme Miranda de Sousa Bispo
-#		João Victor Mendonça Martins
+#		CauÃ£ Ferraz Bittencourt,
+#		JoÃ£o Guilherme Miranda de Sousa Bispo
+#		JoÃ£o Victor MendonÃ§a Martins
 #
 # Pseudo Code:
-# Função strcat(destination, source):
-#    enquanto o caractere atual em destination não for o terminador nulo '\0':
-#        avance o ponteiro de destino para o próximo caractere
-#    enquanto o caractere atual em source não for o terminador nulo '\0':
+# FunÃ§Ã£o strcat(destination, source):
+#    enquanto o caractere atual em destination nÃ£o for o terminador nulo '\0':
+#        avance o ponteiro de destino para o prÃ³ximo caractere
+#    enquanto o caractere atual em source nÃ£o for o terminador nulo '\0':
 #        copie o caractere atual de source para o caractere atual de destino
-#        avance o ponteiro de destino para o próximo caractere
-#        avance o ponteiro de source para o próximo caractere
+#        avance o ponteiro de destino para o prÃ³ximo caractere
+#        avance o ponteiro de source para o prÃ³ximo caractere
 
 #    coloque o caractere '\0' no final de destination
 #    retorne o ponteiro inicial de destination
@@ -24,10 +24,10 @@ main:
 	jal printString
 	
 	# Obtendo a primeira string
-	ori $v0, $zero, 8	# Serviço 8 em v0 indica leitura de string
+	ori $v0, $zero, 8	# ServiÃ§o 8 em v0 indica leitura de string
 	la $a0, str1
 	lw $a1, InputSize
-	syscall			# Salvando o buffer recebido no serviço 8 em str1
+	syscall			# Salvando o buffer recebido no serviÃ§o 8 em str1
 	
 	# Removendo o '\n' da primeira string, se presente
         la $t0, str1
@@ -38,10 +38,10 @@ main:
 	jal printString
 	
 	# Obtendo a segunda string
-	ori $v0, $zero, 8	# Serviço 8 em v0 indica leitura de string
+	ori $v0, $zero, 8	# ServiÃ§o 8 em v0 indica leitura de string
 	la $a0, str2
 	lw $a1, InputSize
-	syscall			# Salvando o buffer recebido no serviço 8 em str2
+	syscall			# Salvando o buffer recebido no serviÃ§o 8 em str2
 	
 	# Removendo o '\n' da primeira string, se presente
         la $t0, str2
@@ -73,13 +73,13 @@ main:
     
 	jal exit
 	
+	
 .data 
-
-	Prompt1: 	.asciiz "Insira uma string (Tamanho máximo: 256 Char): "
-	Prompt2: 	.asciiz "insira agora a String que você deseja concatenar (Tamanho máximo: 256 Char):"
+	Prompt1: 	.asciiz "Insira uma string (Tamanho maximo: 256 Char): "
+	Prompt2: 	.asciiz "Insira a string para concatenar (Tamanho maximo: 256 Char):"
 	OutputStr1:	.asciiz "A primeira string foi: "
-	OutputStr2:	.asciiz "A segunda string foi: "
-	Resultado:	.asciiz "Strings concatenadas: "
+	OutputStr2:	.asciiz "\nA segunda string foi: "
+	Resultado:	.asciiz "\nStrings concatenadas: "
 	str1: 		.space 	256
 	str2: 		.space 	256
 	InputSize:	.word 	256
@@ -87,48 +87,49 @@ main:
 
 
 # Subprograma:		strcat
-# Propósito:		Concatenação de duas Strings 
-# Input:		$a0 - endereço de memória destino
-#			$a1 - endereço de memória origem
-# Retorno:		$v0 - endereço de memória destino
+# PropÃ³sito:		ConcatenaÃ§Ã£o de duas Strings 
+# Input:		$a0 - endereÃ§o de memÃ³ria destino
+#			$a1 - endereÃ§o de memÃ³ria origem
+# Retorno:		$v0 - endereÃ§o de memÃ³ria destino
 .text 
 strcat:
-	# separa espaço na pilha para evitar sobreescrição
-	addi $sp, $sp, -8       # Aloca espaço na pilha para o endereço de retorno e $s0
-        sw   $ra, 0($sp)        # Armazena o endereço de retorno
+	# separa espaÃ§o na pilha para evitar sobreescriÃ§Ã£o
+	addi $sp, $sp, -8       # Aloca espaÃ§o na pilha para o endereÃ§o de retorno e $s0
+        sw   $ra, 0($sp)        # Armazena o endereÃ§o de retorno
         sw   $s0, 4($sp)        # Armazena $s0
         
-        # Copia endereço para registragdores de trabalho 
-        move $s0, $a0           # $s0 vai manter o endereço base de destination para retorno
+        # Copia endereÃ§o para registragdores de trabalho 
+        move $s0, $a0           # $s0 vai manter o endereÃ§o base de destination para retorno
 	move $t0, $a0           # $t0 vai ser usado para navegar pelo destino
 	move $t1, $a1           # $t1 vai ser usado para navegar pela origem
 	
 	
-	# Função que acha o fim de destination 
+	# FunÃ§Ã£o que acha o fim de destination 
 	loop_find_end:
-		lb $t2, 0($t0) #carrega byte atual de destination em $t2
-		beq $t2, $zero, loop_copy # se encontar o final de destination(NULL) começar a copiar a string
-		addi $t0, $t0, 1 # aponta para o próximo byte em $t0
-		b loop_find_end # repete o loop até encontrar o NULL
+		lb   $t2, 0($t0) 		# carrega byte atual de destination em $t2
+		beq  $t2, $zero, loop_copy 	# se encontar o final de destination(NULL) comeÃ§ar a copiar a string
+		addi $t0, $t0, 1 		# aponta para o prÃ³ximo byte em $t0
+		b loop_find_end 		# repete o loop atÃ© encontrar o NULL
 		
-	# Função que copia source para destination
+	# FunÃ§Ã£o que copia source para destination
 	loop_copy: 
-		lb $t2, 0($t1) # pega o primeiro byte de source 
-		sb $t2, 0($t0) # salva o primeiro byte de source no final de destination 
-		beq $t2, $zero, end_strcat # quando chegar no final de source, terminar o programa 
-		addi $t0, $t0, 1 # Move para o próximo byte em destination
-		addi $t1, $t1, 1 # Move para o próximo byte em source
-		b loop_copy # repete o loop até todo o source ser copiado em destination 
+		lb   $t2, 0($t1) 		# pega o primeiro byte de source 
+		sb   $t2, 0($t0) 		# salva o primeiro byte de source no final de destination 
+		beq  $t2, $zero, end_strcat 	# quando chegar no final de source, terminar o programa 
+		addi $t0, $t0, 1 		# Move para o prÃ³ximo byte em destination
+		addi $t1, $t1, 1 		# Move para o prÃ³ximo byte em source
+		b loop_copy 			# repete o loop atÃ© todo o source ser copiado em destination 
 		
-	
-	# Função que retorna e finaliza o código
+	# FunÃ§Ã£o que retorna e finaliza o cÃ³digo
 	end_strcat:
-		sb $zero, 0($t0)    # Coloca o caractere nulo '\0' no final
-		move $v0, $s0 # move o valor de destination para $v0 que é usado para retorno de fução 
-		lw $ra, 0($sp) # Recupera endereço de retorno colocado na pilha no início
-		lw $s0, 4($sp) # Recupera o valor de s0 que é o endereço de destination
-		addi $sp, $sp, 8 # libera espaço na pilha 
+		sb   $zero, 0($t0)  	# Coloca o caractere nulo '\0' no final
+		move $v0, $s0 		# move o valor de destination para $v0 que Ã© usado para retorno de fuÃ§Ã£o 
+		lw   $ra, 0($sp) 	# Recupera endereÃ§o de retorno colocado na pilha no inÃ­cio
+		lw   $s0, 4($sp) 	# Recupera o valor de s0 que Ã© o endereÃ§o de destination
+		addi $sp, $sp, 8 	# libera espaÃ§o na pilha 
 		
-		jr $ra # retorna o endereço para a função chamadora 
+		jr   $ra 		# retorna o endereÃ§o para a funÃ§Ã£o chamadora 
+		
 		
 .include "utils.asm"
+
