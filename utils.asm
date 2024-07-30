cf6a98f939352fd6dfa68b6db01c193e0064f9a4
Higor Cordeiro <108764837+higorm7@users.noreply.github.com>
2024-07-30 10:52:38 -0300

    Update utils.asm - Removed comparaTamanhoComLimite
    Função removida não foi útil para o desenvolvimento de strncmp.asm

--- utils.asm
+++ utils.asm
@@ -1,58 +1,58 @@
 # Arquivo:	utils.asm
-# Propósito: Fornecer utilitários comuns para implementação nas
-#			funções solicitadas.
+# PropÃ³sito: Fornecer utilitÃ¡rios comuns para implementaÃ§Ã£o nas
+#			funÃ§Ãµes solicitadas.
 # Autores: 	Higor Matheus da Costa Cordeiro, 
-#			Cauã Ferraz Bittencourt,
-#			João Guilherme Miranda de Sousa Bispo
-#			João Victor Mendonça Martins
+#			CauÃ£ Ferraz Bittencourt,
+#			JoÃ£o Guilherme Miranda de Sousa Bispo
+#			JoÃ£o Victor MendonÃ§a Martins
 #
 # Index de Subprogramas:
 #	printString -	Imprime uma String
 #	printInt -		Imprime um inteiro
 #	strlen -		Calcula o tamanho de uma string
-#	comparaTamanho -			Compara os tamanhos das strings informadas pelo usuário
-# 	comparaTamanhoComLimite - 	Análogo à comparaTamanho, com a imposição de um número limite de caracteres
+#	comparaTamanho -			Compara os tamanhos das strings informadas pelo usuÃ¡rio
+# 	comparaTamanhoComLimite - 	AnÃ¡logo Ã  comparaTamanho, com a imposiÃ§Ã£o de um nÃºmero limite de caracteres
 #	exit -			Encerra o programa
 
 
 # Subprograma:		printString
-# Propósito:		Imprimir strings
-# Input:			$a0 - endereço da string a ser impressa
-# Retorno:			Não se aplica
-# Side effects:		A string passada como argumento é impressa
+# PropÃ³sito:		Imprimir strings
+# Input:			$a0 - endereÃ§o da string a ser impressa
+# Retorno:			NÃ£o se aplica
+# Side effects:		A string passada como argumento Ã© impressa
 .text
 printString:
-	ori $v0, $zero, 4	# Serviço 4 indica impressão de Strings
+	ori $v0, $zero, 4	# ServiÃ§o 4 indica impressÃ£o de Strings
 	syscall
 	
-	jr  $ra				# Retorna à função que a chamou
+	jr  $ra				# Retorna Ã  funÃ§Ã£o que a chamou
 
 
 # Subprograma:		printInt
-# Propósito:		Imprimir inteiros
-# Input:			$a0 - endereço do inteiro a ser impresso
-# Retorno:			Não se aplica
-# Side effects:		O inteiro passado como argumento é impresso
+# PropÃ³sito:		Imprimir inteiros
+# Input:			$a0 - endereÃ§o do inteiro a ser impresso
+# Retorno:			NÃ£o se aplica
+# Side effects:		O inteiro passado como argumento Ã© impresso
 .text
 printInt:
-	ori $v0, $zero, 1	# Serviço 1 indica impressão de inteiros
+	ori $v0, $zero, 1	# ServiÃ§o 1 indica impressÃ£o de inteiros
 	syscall		
 	
-	jr  $ra				# Retorna à função que a chamou
+	jr  $ra				# Retorna Ã  funÃ§Ã£o que a chamou
 
 
 # Subprograma:		strlen
-# PropÃ³sito:		Contar a quantidade de caracteres numa string
+# PropÃƒÂ³sito:		Contar a quantidade de caracteres numa string
 # Input:			$a0 - Endereco da string alvo
 # Retorno:			$v0 - Quantidade de caracteres na string
 # Side effects:		Nao se aplicam
 .text
 strlen:
-	addi $sp, $sp, -8		# Alocando 8 bytes na pilha para guardar o endereÃ§o de retorno e $s0
-	sw   $ra, 0($sp)		# Guardando o endereço de retorno na pilha
-	sw   $s0, 4($sp)		# Guardando o valor de $s0 para ficar disponÃível nesta função
+	addi $sp, $sp, -8		# Alocando 8 bytes na pilha para guardar o endereÃƒÂ§o de retorno e $s0
+	sw   $ra, 0($sp)		# Guardando o endereÃ§o de retorno na pilha
+	sw   $s0, 4($sp)		# Guardando o valor de $s0 para ficar disponÃƒÃ­vel nesta funÃ§Ã£o
 
-	move $t0, $a0			# Copia o endereÃ§o da string para $t0
+	move $t0, $a0			# Copia o endereÃƒÂ§o da string para $t0
 	addi $s0, $zero, 0		# Inicializa o contador da string
 	
 	strlen_loop:
@@ -60,7 +60,7 @@ strlen:
 		sne  $t2, $t1, $zero	# Compara o caractere em $t1 com '\0' e salva o booleano em $t2
 		beqz $t2, strlen_end	# Se $t2 for zero, branch para strlen_end
 			addi $s0, $s0, 1		# Adiciona o contador da string
-			addi $t0, $t0, 1		# Adiciona 1 byte ao endereÃ§o contido em $t0: 
+			addi $t0, $t0, 1		# Adiciona 1 byte ao endereÃƒÂ§o contido em $t0: 
 									# segue para o proximo caractere
 		b strlen_loop			# Recomeca o loop
 		
@@ -117,95 +117,33 @@ comparaTamanho:
 		jr   $ra			# Retorna ao programa que o chamou
 
 
-# Subprograma:		comparaTamanhoComLimite
-# PropÃ³sito:		Verificar se o tamanho de duas strings, dado um tamanho limite n, é igual
-# Input:			$a0 - 	endereço de str1
-#					$a1 - 	endereço de str2
-#					$a2 - 	tamanho limite
-# Retorno:			$v0 - 	0, se as strings tiverem o mesmo tamanho
-#							-1, se str2 for maior
-#							1, se str1 for maior
-# Side effects:		Não se aplica
-.text
-comparaTamanhoComLimite:
-	addi $sp, $sp, -20	# Aloca 20 bytes na pilha para armazenamento de valores
-	sw   $ra, 0($sp)	# Armazena o valor de retorno da função
-	sw   $a0, 4($sp)	# Armazena o endereço de str1
-	sw   $a1, 8($sp)	# Armazena o endereço de str2
-	sw   $a2, 12($sp)	# Armazena o limite de caracteres
-	sw   $s0, 16($sp)	# Armazena $s0 para uso neste subprograma
-	
-	# Calcula o tamanho de str1 e armazena em $s0
-	jal  strlen			
-	move $s0, $v0
-	
-	# Calcula o tamanho de str2 após recupera-la na pilha e o armazena em $s1
-	lw   $a0, 8($sp)
-	jal  strlen
-	move $s1, $v0
-	
-	# Recupera o valor limite armazenado na pilha e o subtrai do tamanho de str1 e do tamanho de str2
-	lw   $t0, 12($sp)
-	sub  $s0, $s0, $t0
-	sub  $s1, $s1, $t0
-	
-	# Se, apos subtrair o valor limite, o tamanho de str1 e maior, branch para greater
-	sgt  $t0, $s0, $s1
-	beq  $t0, 1, greater
-	
-	# Se, apos subtrair o valor limite, o tamanho de str1 e menor, branch para lesser
-	slt  $t1, $s0, $s1
-	beq  $t1, 1, lesser
-	
-	# Se nenhum dos dois anteriores ocorreu, so podem ser iguais
-	b    equal_tam
-	
-	greater:
-		# Se for maior, adiciona 1 ao valor de retorno
-		addi $v0, $zero, 1
-		b    end_compara
-	lesser:
-		# Se for menor, adiciona -1 ao valor de retorno
-		addi $v0, $zero, -1
-		b    end_compara
-	equal_tam:
-		# Se forem iguais, adiciona 0 ao valor de retorno
-		addi $v0, $zero, 0
-	end_compara:
-		lw   $ra, 0($sp)	# Recupera o valor de retorno da função
-		lw   $s0, 16($sp)	# Recupera o valor de $s0 da pilha
-		addi $sp, $sp, 20	# Devolve a memoria alocada à pilha
-		
-		jr $ra				# Retorna ao programa queo chamou
-
-
 # Subprograma: removeNewline
-# Propósito: Remover o caractere de nova linha de uma string, se presente
-# Input: $t0 - endereço da string
+# PropÃ³sito: Remover o caractere de nova linha de uma string, se presente
+# Input: $t0 - endereÃ§o da string
 .text
 removeNewline:
     loop_remove:
         lb $t1, 0($a0)      # Carrega o byte atual da string
         beq $t1, $zero, end_remove # Se encontrar o final da string (NULL), terminar
         beq $t1, 10, remove_char # Se encontrar o caractere de nova linha (ASCII 10), remover
-        addi $a0, $a0, 1    # Aponta para o próximo byte em $a0
-        b loop_remove       # Repete o loop até encontrar o NULL
+        addi $a0, $a0, 1    # Aponta para o prÃ³ximo byte em $a0
+        b loop_remove       # Repete o loop atÃ© encontrar o NULL
         
     remove_char:
         sb $zero, 0($a0)    # Substitui o caractere de nova linha por NULL
-        jr $ra              # Retorna para a função chamadora
+        jr $ra              # Retorna para a funÃ§Ã£o chamadora
     
     end_remove:
-        jr $ra              # Retorna para a função chamadora
+        jr $ra              # Retorna para a funÃ§Ã£o chamadora
 
 
 # Subprograma:		exit
-# PropÃ³sito:		Finalizar o programa
-# Input:			Não se aplica
-# Retorno:			Não se aplica
+# PropÃƒÂ³sito:		Finalizar o programa
+# Input:			NÃ£o se aplica
+# Retorno:			NÃ£o se aplica
 # Side effects:		Encerra o programa
 .text
 exit:
-	ori  $v0, $zero, 10	# Serviço 10 indica encerramento do programa
+	ori  $v0, $zero, 10	# ServiÃ§o 10 indica encerramento do programa
 	syscall
 	
