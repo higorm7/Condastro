ef79191ccdf5eb8eb95a8597189ee5bca3d41e02
Higor Cordeiro <108764837+higorm7@users.noreply.github.com>
2024-07-31 09:16:41 -0300

    Update strcat.asm
    Formatação do código

--- strcat.asm
+++ strcat.asm
@@ -1,77 +1,77 @@
 # Arquivo:	strcat.asm
-# PropÃ³sito: 	ConcatenaÃ§Ã£o de duas Strings 
+# Proposito: 	Concatenacao de duas Strings 
 # Autores: 	Higor Matheus da Costa Cordeiro, 
-#		CauÃ£ Ferraz Bittencourt,
-#		JoÃ£o Guilherme Miranda de Sousa Bispo
-#		JoÃ£o Victor MendonÃ§a Martins
+#		Caua Ferraz Bittencourt,
+#		Joao Guilherme Miranda de Sousa Bispo
+#		Joao Victor Mendonca Martins
 #
 # Pseudo Code:
-# FunÃ§Ã£o strcat(destination, source):
-#    enquanto o caractere atual em destination nÃ£o for o terminador nulo '\0':
-#        avance o ponteiro de destino para o prÃ³ximo caractere
-#    enquanto o caractere atual em source nÃ£o for o terminador nulo '\0':
+# Funcao strcat(destination, source):
+#    enquanto o caractere atual em destination nao for o terminador nulo '\0':
+#        avance o ponteiro de destino para o proximo caractere
+#    enquanto o caractere atual em source nao for o terminador nulo '\0':
 #        copie o caractere atual de source para o caractere atual de destino
-#        avance o ponteiro de destino para o prÃ³ximo caractere
-#        avance o ponteiro de source para o prÃ³ximo caractere
-
+#        avance o ponteiro de destino para o proximo caractere
+#        avance o ponteiro de source para o proximo caractere
 #    coloque o caractere '\0' no final de destination
 #    retorne o ponteiro inicial de destination
+
 
 .text 
 main:
 	# Imprimindo o prompt
-	la  $a0, Prompt1
-	jal printString
+	la   $a0, Prompt1
+	jal  printString
 	
 	# Obtendo a primeira string
-	ori $v0, $zero, 8	# ServiÃ§o 8 em v0 indica leitura de string
-	la $a0, str1
-	lw $a1, InputSize
-	syscall			# Salvando o buffer recebido no serviÃ§o 8 em str1
+	ori  $v0, $zero, 8	# Servico 8 em v0 indica leitura de string
+	la   $a0, str1
+	lw   $a1, InputSize
+	syscall			# Salvando o buffer recebido no servico 8 em str1
 	
 	# Removendo o '\n' da primeira string, se presente
-        la $t0, str1
-        jal removeNewline
+        la   $t0, str1
+        jal  removeNewline
 	
 	# Imprimindo Prompt2
-	la $a0, Prompt2
-	jal printString
+	la   $a0, Prompt2
+	jal  printString
 	
 	# Obtendo a segunda string
-	ori $v0, $zero, 8	# ServiÃ§o 8 em v0 indica leitura de string
-	la $a0, str2
-	lw $a1, InputSize
-	syscall			# Salvando o buffer recebido no serviÃ§o 8 em str2
+	ori  $v0, $zero, 8	# Servico 8 em v0 indica leitura de string
+	la   $a0, str2
+	lw   $a1, InputSize
+	syscall			# Salvando o buffer recebido no servico 8 em str2
 	
 	# Removendo o '\n' da primeira string, se presente
-        la $t0, str2
-        jal removeNewline
+        la   $t0, str2
+        jal  removeNewline
 	
 	# Imprimindo as strings recebidas
-	la $a0, OutputStr1
-	jal printString
-	la $a0, str1
-	jal printString
-	la $a0, OutputStr2
-	jal printString
-	la $a0, str2
-	jal printString
+	la   $a0, OutputStr1
+	jal  printString
+	la   $a0, str1
+	jal  printString
+	la   $a0, OutputStr2
+	jal  printString
+	la   $a0, str2
+	jal  printString
 	
 	# Obtendo o resultado
-	la $a0, str1
-	la $a1, str2
-	jal strcat
+	la   $a0, str1
+	la   $a1, str2
+	jal  strcat
 	move $s0, $v0
 	
 	# Imprimindo o resultado
-	la $a0, Resultado
-	jal printString
+	la   $a0, Resultado
+	jal  printString
 	
 	# Passando o resultado como argumento de printInt, e, em seguida, o imprimindo
 	move $a0, $s0
-	jal printString
+	jal  printString
     
-	jal exit
+	jal  exit
 	
 	
 .data 
@@ -83,7 +83,6 @@ main:
 	str1: 		.space 	256
 	str2: 		.space 	256
 	InputSize:	.word 	256
-
 
 
 # Subprograma:		strcat
@@ -109,7 +108,7 @@ strcat:
 		lb   $t2, 0($t0) 		# carrega byte atual de destination em $t2
 		beq  $t2, $zero, loop_copy 	# se encontar o final de destination(NULL) comeÃ§ar a copiar a string
 		addi $t0, $t0, 1 		# aponta para o prÃ³ximo byte em $t0
-		b loop_find_end 		# repete o loop atÃ© encontrar o NULL
+		b    loop_find_end 		# repete o loop atÃ© encontrar o NULL
 		
 	# FunÃ§Ã£o que copia source para destination
 	loop_copy: 
@@ -118,7 +117,7 @@ strcat:
 		beq  $t2, $zero, end_strcat 	# quando chegar no final de source, terminar o programa 
 		addi $t0, $t0, 1 		# Move para o prÃ³ximo byte em destination
 		addi $t1, $t1, 1 		# Move para o prÃ³ximo byte em source
-		b loop_copy 			# repete o loop atÃ© todo o source ser copiado em destination 
+		b    loop_copy 			# repete o loop atÃ© todo o source ser copiado em destination 
 		
 	# FunÃ§Ã£o que retorna e finaliza o cÃ³digo
 	end_strcat:
