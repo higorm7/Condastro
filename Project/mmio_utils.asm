e91583b01b515d1eac0148a3f3141f46acbd9b33
Higor Cordeiro <108764837+higorm7@users.noreply.github.com>
2024-08-01 09:52:22 -0300

    Update mmio_utils.asm - Adicionadas novas funções (printBanner e getInput)
    A função printBanner será um utilitário para uso quando for necessário imprimir o banner do shell. getInput será utilizada para receber uma string de um usuário e encerrar o recebimento quando for digitado o caractere de newLine (\n)

--- Project/mmio_utils.asm
+++ Project/mmio_utils.asm
@@ -8,7 +8,9 @@
 #
 # Index de Subprogramas:
 #	printString -		Imprime uma String
-#	exit -				Encerra o programa
+#	printBanner -		Imprime o banner do shell
+#	getInput -		Recebe uma string como input
+#	exit -			Encerra o programa
 
 
 # Subprograma:		printString
@@ -29,7 +31,7 @@ printString:
     	beqz $t2, end		# Se for o caractere nulo, finaliza o loop
 
 	wait_ready:
-    	lw   $t3, 0($t0)		# Carrega o valor do registrador de controle de transmissao
+    	lw   $t3, 0($t0)	# Carrega o valor do registrador de controle de transmissao
     	andi $t3, $t3, 0x1  	# Isola o bit de ready do registrador
     	beqz $t3, wait_ready	# Se for zero, aguarda disponibilidade do transmissor
 
@@ -43,10 +45,82 @@ printString:
     	jr   $ra			# Retorna a funcao que chamou
 
 
-# Secao de memoria estatica para armazenamento dos enderecos dos MMIO
+# Subprograma:		printBanner
+# Proposito:		Imprimir o banner do shell
+# Input:		Nao se aplica
+# Retorno:		Nao se aplica
+# Side effects:		A string do banner e impressa
+.text
+printBanner:
+	addi $sp, $sp, -4	# Aloca 4 bytes na pilha
+	sw   $ra, 0($sp)	# Armazena o endereco de retorno da funcao na pilha
+	
+	la   $a0, banner	# Armazena o endereco da string de banner como argumento de printString
+	jal  printString	# Chama a funcao de impressao de strings
+	
+	lw   $ra, 0($sp)	# Recupera o endereco de retorno da funcao
+	addi $sp, $sp, 4	# Devolve os 4 bytes alocados a pilha
+	
+	jr   $ra		# retorna a funcao que a chamou
+	
+	
+# Secao estatica para armazenamento da string de banner
 .data
-	transmitter_control: .word 0xffff0008	# endereco do Controle de Transmissao
-	transmitter_data:    .word 0xffff000c   # Endereco dos dados de transmissao
+	banner: .asciiz	"CHJJ-shell>> "	
+
+
+# Subprograma:		getInput
+# Proposito:		Receber um input do usuario
+# Input:		Nao se aplica
+# Retorno:		$v0 - string digitada pelo usuario
+# Side effects:		Nao se aplica
+.text
+getInput:
+	addi $sp, $sp, -4	# Aloca 4 bytes na memoria da pilha
+	sw   $ra, 0($sp)	# Armazena o endereco de retorno da funcao pra recuperacao posterior
+	
+	lw   $t0, receiver_control	# Armazena o endereco do registrador de controle do teclado em $t0
+	lw   $t1, receiver_data		# Armazena o endereco do registrador de dados do teclado em $t1
+	lw   $t2, transmitter_control	# Armazena o endereco do registrador de controle do display em $t2
+	lw   $t3, transmitter_data	# Armazena o endereco do registrador de dados do display em $t3
+	la   $t4, buffer		# Armazena o endereco do buffer em $t4
+	
+	# Bloco de leitura do caractere
+	read_char:
+		lw   $t5, 0($t0)	# Armazena o valor do registrador de controle
+		andi $t5, $t5, 0x1	# Isola o bit ready
+		beqz $t5, read_char	# Se ready for 0, aguarda fornecimento do caractere
+		
+		lb   $t5, 0($t1)	# Se houver caractere, o armazenam em $t5
+		sb   $t5, 0($t4)	# Armazena o caractere no buffer
+		
+	# Bloco de espera de disponibilidade do display
+	wait_ready_transmitter:
+		lw   $t5, 0($t2)	# Armazena o valor do registrador de controle de display
+		andi $t5, $t5, 0x1	# Isola o bit ready
+		beqz $t5, wait_ready_transmitter	# Se nao estiver disponivel, aguarda disponibilidade
+		
+	# Bloco de impressao do caractere recebido
+	print_char:
+		lb   $t5, 0($t4)	# Armazena o valor para imprimir em $t5
+		sb   $t5, 0($t3)	# Armazena o valor de impressao no registrador de dados do display e o imprime
+		beq  $t5, 0x0a, end_input	# Se o caractere for igual a newLine, encerra o input
+		addi $t4, $t4, 1	# Incrementa o index do buffer
+	
+	
+	b  read_char	# Recomeca o loop
+	
+	end_input: 
+		lw   $ra, 0($sp)	# Recupera o endereco de retorno da funcao
+		addi $sp, $sp, 4	# Devolve os 4 bytes alocados da memoria
+		la   $v0, buffer	# Retorna o buffer em $v0
+	
+	jr   $ra		# Retorna para a funcao que o chamou
+	
+
+# Secao de dados estaticos para o armazenamento do buffer
+.data
+	buffer:	.space 256
 
 
 # Subprograma:		exit
@@ -56,5 +130,15 @@ printString:
 # Side effects:		A string passada como argumento e impressa
 .text
 exit:
-	ori $v0, $zer0, 10
+	ori $v0, $zero, 10	# Servico 10 indica encerramento de programa
 	syscall
+
+
+# Secao de memoria estatica para armazenamento dos enderecos dos MMIO
+.data
+	transmitter_control: .word 0xffff0008	# Endereco do Controle de Transmissao
+	transmitter_data:    .word 0xffff000c   # Endereco dos dados de transmissao
+	receiver_control:    .word 0xffff0000   # Endereco do Controle de Receptor
+	receiver_data:	     .word 0xffff0004	# Endereco dos dados de Receptor
+	
+	
