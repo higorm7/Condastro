9413e35ef7da9c2f7fb5e4b2ac29ffe6734d9b7e
Higor Cordeiro <higormatheus1618@gmail.com>
2024-08-01 19:10:27 -0300

    Added static.asm: reservando memória para strings de comando e erro

---
+++ Project/static.asm
@@ -0,0 +1,20 @@
+# Arquivo:		static.asm
+# Proposito: 	Conter a secao de memoria estatica para uso no projeto. Contem: strings de erro e comando.
+# Autores: 		Higor Matheus da Costa Cordeiro, 
+#				Caua Ferraz Bittencourt,
+#				Joao Guilherme Miranda de Sousa Bispo
+#				Joao Victor Mendonca Martins
+
+
+.data
+	cmd_1: 			.asciiz "addMorador"
+	cmd_2: 			.asciiz "rmvMorador"
+	cmd_3: 			.asciiz "addAuto"
+	cmd_4: 			.asciiz "rmvAuto"
+	cmd_5: 			.asciiz "limparAp"
+	cmd_6: 			.asciiz "infoAp"
+	cmd_7: 			.asciiz "infoGeral"
+	cmd_8: 			.asciiz "salvar"
+	cmd_9: 			.asciiz "recarregar"
+	cmd_10: 		.asciiz "formatar"
+	invalid_cmd: 	.asciiz "Comando invalido"
