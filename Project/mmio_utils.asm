4a9d6334f6541dd31016315f641dc4e12d1fdcdf
Higor Cordeiro <higormatheus1618@gmail.com>
2024-08-02 20:08:00 -0300

    Adicionada função getOptions: realiza a leitura de options em cada comando e os escreve em options. Não está completa, verificar a issue criar getOptions para mais detalhes.

--- Project/mmio_utils.asm
+++ Project/mmio_utils.asm
@@ -166,6 +166,80 @@ getCommand:
 		
 	jr   $ra			# Retorna para a funcao que o chamou
 	
+	
+# Subprograma:		getOptions
+# Proposito:		Extrair as options de um comando
+# Input:			$a0 - String do comando
+# Retorno:			$v0 - endereco do array de Options
+# Side effects:		Nao se aplica
+.text
+getOptions:
+	addi $sp, $sp, -8	# Aloca 8 bytes na pilha
+	sw   $ra, 0($sp)	# Armazena o endereco de retorno
+	sw   $s0, 4($sp)	# Armazena o valor de s0 na pilha
+
+	move $t0, $a0		# Armazena a string de comando no $t0
+	la   $s0, options	# Armazena o endereco de options em $s0
+	
+	# Loop para encontrar o prefixo " --" nas options
+	loopFindPrefix:
+		lb   $t1, 0($t0)		# Carrega o primeiro caractere da string
+		beqz $t1, endGetOptions	# Se for o caractere nulo, finaliza
+		
+		seq  $t1, $t1, 0x20		# Se o caractere for igual a espaço, retorna 1
+		beqz $t1, restart		# Se não for, recomeça o loop
+	
+		lb   $t2, 1($t0)		# Carrega o próximo caractere
+		seq  $t2, $t2, 0x2d		# Se o caractere for igual a '-', retorna 1
+	
+		and  $t1, $t1, $t2		# Se os dois forem os caracteres desejados, retorna 1
+		beqz $t1, restart		# Se não forem, recomeça o loop
+	
+		lb   $t2, 2($t0)		# Carrega o próximo caractere
+		seq  $t2, $t2, 0x2d		# Se o caractere for igual a '-', retorna 1
+	
+		and  $t1, $t1, $t2		# Se os três forem os caracteres desejados, retorna 1
+		bnez $t1, equal			# Se forem os caracteres desejados, finaliza o loop
+	
+	# Reinicio do loop
+	restart:
+		addi $t0, $t0, 1		# Incrementa o index da string
+		b    loopFindPrefix		# Recomeça o loop
+	
+	# Incremento do index da string apos encontrar o prefixo
+	equal:
+		addi $t0, $t0, 3		# Se for encontrado o " --", incrementar em 3 o index da string e começar a extrair
+	
+	# Armazena os caracteres encontrados em options	
+	get:
+		lb  $t1, 0($t0)        			# Carrega o caractere apos encontrar o " --"
+		beq $t1, $zero, end_getting		# Se for o caractere nulo, encerra o loop
+		beq $t1, 0x20, end_getting    	# Se for um espaco, encerra o loop
+
+		sb   $t1, 0($s0)		# Armazena o caractere em $s0
+		addi $s0, $s0, 1      	# Incrementa o index de options
+		addi $t0, $t0, 1		# Incrementa o index da string
+
+		b    get				# Extrai os caracteres ate encontrar um espaco ou fim de string
+	
+	# Finaliza o processo de obtencao de options
+	end_getting:
+		b    loopFindPrefix		# Recomeca o processo de encontrar options
+	
+	# Encerra e retorna ao programa
+	endGetOptions:
+		lw   $ra, 0($sp)	# Recupera o valor de $ra na pilha
+		lw   $s0, 4($sp)	# Recupera o valor de $s0 na pilha
+		addi $sp, $sp, 8	# Devolve a pilha a memoria alocada
+	
+		la   $v0, options	# Retorna o endereco de options 
+		jr 	 $ra			# Retorna a funcao que o chamou
+		
+		
+# Secao de memoria estatica para uso em getOptions		
+.data
+	options: .space 150
+
 
 # Secao de dados estaticos para o armazenamento do buffer
 .data
