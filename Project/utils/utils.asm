4e4160dcc3ac93d11b9aea1b4e5b87c246541ca8
Higor Cordeiro <108764837+higorm7@users.noreply.github.com>
2024-08-08 15:20:05 -0300

    Update utils.asm
    Finalizado infoGeral. Contém alguns testes necessários pois calculateApartAddress está incorreto.

--- Project/utils/utils.asm
+++ Project/utils/utils.asm
@@ -1,213 +1,315 @@
-# Arquivo:	utils.asm
-# PropÃƒÆ’Ã‚Â³sito: 	Fornecer utilitarios comuns para implementacao nas
-#		funcoes solicitadas.
-# Autores: 	Higor Matheus da Costa Cordeiro, 
-#		Caua Ferraz Bittencourt,
-#		Joao Guilherme Miranda de Sousa Bispo
-#		Joao Victor Mendonca Martins
-#
-# Index de Subprogramas:
-#	printString -			Imprime uma String
-#	printInt -			Imprime um inteiro
-#	strlen -			Calcula o tamanho de uma string
-#	comparaTamanho -		Compara os tamanhos das strings informadas pelo usuario
-#   strcmp -			Compara duas strings
-#	exit -				Encerra o programa
-
-
-# Subprograma:		printString
-# Proposito:		Imprimir strings
-# Input:			$a0 - endereco da string a ser impressa
-# Retorno:			Nao se aplica
-# Side effects:		A string passada como argumento e impressa
 .text
-printString:
-	ori $v0, $zero, 4	# Servico 4 indica impressao de Strings
-	syscall
+main:
+	exec:
+		# Bloco de impressao do banner e obtencao de input
+		jal  printBanner	# Imprime o banner
+		jal  getInput		# Obtem o comando do usuario
+		move $s1, $v0		# Armazena o input em $s1
+		move $a0, $s1		# Passa o input como parametro para removeNewline
+		jal removeNewline	# Remove o caractere de newLine
+		move $s1, $v0		# Armazena o valor em $s1
 	
-	jr  $ra			# Retorna a funcao que a chamou
-
-
-# Subprograma:		printInt
-# Proposito:		Imprimir inteiros
-# Input:		$a0 - endereco do inteiro a ser impresso
-# Retorno:		Nao se aplica
-# Side effects:		O inteiro passado como argumento e impresso
-.text
-printInt:
-	ori $v0, $zero, 1	# Servico 1 indica impressao de inteiros
-	syscall		
-	
-	jr  $ra			# Retorna a funcao que a chamou
-
-
-# Subprograma:		strlen
-# Proposito:		Contar a quantidade de caracteres numa string
-# Input:		$a0 - Endereco da string alvo
-# Retorno:		$v0 - Quantidade de caracteres na string
-# Side effects:		Nao se aplicam
-.text
-strlen:
-	addi $sp, $sp, -8		# Alocando 8 bytes na pilha para guardar o endereco de retorno e $s0
-	sw   $ra, 0($sp)		# Guardando o endereco de retorno na pilha
-	sw   $s0, 4($sp)		# Guardando o valor de $s0 para ficar disponivel nesta funcao
-
-	move $t0, $a0			# Copia o endereco da string para $t0
-	addi $s0, $zero, 0		# Inicializa o contador da string
-	
-	strlen_loop:
-		lb   $t1, 0($t0)		# Inicializa $t1 com o caractere da string
-		sne  $t2, $t1, $zero		# Compara o caractere em $t1 com '\0' e salva o booleano em $t2
-		beqz $t2, strlen_end		# Se $t2 for zero, branch para strlen_end
-			addi $s0, $s0, 1		# Adiciona o contador da string
-			addi $t0, $t0, 1		# Adiciona 1 byte ao endereÃƒÆ’Ã†â€™Ãƒâ€šÃ‚Â§o contido em $t0: 
-							# segue para o proximo caractere
-		b strlen_loop			# Recomeca o loop
+		move $a0, $s1		# Armazena o input em $a0, para ser usado como parametro de getCommand
+		jal  getCommand		# Obtem o comando apos o input
+		move $a0, $v0		# Armazena o comando em $s0
+		jal removeNewline	# Remove o caractere de nova linha do comando
+		move $s0, $v0		# Armazena o resultado em $s0
 		
-	strlen_end:
-		move $v0, $s0			# Copia a quantidade de caracteres encontrada no registrador de retorno
-		lw   $ra, 0($sp)		# Recupera o valor de retorno
-		lw   $s0, 4($sp)		# Recupera o valor guardado em $s0
-		addi $sp, $sp, 8		# Devolve a memoria a pilha
 		
-		jr   $ra				# Retorna ao programa que o chamou
+		# Case addMorador:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_1		# Passa o comando 1 como parametro para strcmp
+		jal  strcmp			# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, addMorador	# Se v0 retornar 0, jump para addMorador
+		
+		# Case rmvMorador:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_2		# Passa o comando 2 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, rmvMorador	# Se v0 retornar 0, jump para rmvMorador
+		
+		# Case addAuto:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_3		# Passa o comando 3 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, addAuto	# Se v0 retornar 0, jump para addAuto
+		
+		# Case rmvAuto
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_4		# Passa o comando 4 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, rmvAuto	# Se v0 retornar 0, jump para rmvAuto
+		
+		# Case limparAp
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_5 	# Passa o comando 5 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, limparAp	# Se v0 retornar 0, jump para limparAp
+		
+		# Case infoAp:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_6		# Passa o comando 6 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, infoAp	# Se v0 retornar 0, jump para infoAp
+		
+		# Case infoGeral:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_7		# Passa o comando 7 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, infoGeral	# Se v0 retornar 0, jump para infoGeral
+		
+		# Case salvar:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_8		# Passa o comando 8 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, salvar	# Se v0 retornar 0, jump para salvar
+		
+		# Case recarregar:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_9		# Passa o comando 9 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, recarregar	# Se v0 retornar 0, jump para recarregar
+		
+		# Case formatar:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_10	# Passa o comando 10 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, formatar	# Se v0 retornar 0, jump para formatar
+		
+		# Case finalizar:
+		move $a0, $s0		# Passa a string como parametro para strcmp		
+		la   $a1, cmd_11	# Passa o comando 11 como parametro para strcmp
+		jal  strcmp		# Compara as strings
+		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		beqz $t0, finalizar	# Se v0 retornar 0, jump para finalizar
+		
+		# Default:
+		b error			# Branch para Comando Invalido
+		
+		# Bloco de addMorador
+		addMorador:
+			# Obtem as options
+			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
+			jal  getOptions		# Obtem as options
+			move $s2, $v0		# Armazena o endereco das options em $s2
+			
+			# Erro de opcoes invalidas
+			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions			# Conta a quantidade de options
+			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			bne  $t0, 2, errorInvalidOptions	# Se houver mais que duas options, imprime erro de Options
+			
+			# Converte strings numericas para inteiros
+			move $a0, $s2				# Passa $s2 como parametro para optionToInt
+			jal  optionToInt			# Transforma o valor contido na primeira option em inteiro
+			move $s3, $v0				# Armazena o retorno em $s3
+			beqz $s3, errorInvalidOptions		# Se o retorno for igual a 0, houve erro de caracteres
+			
+			# Erro de apartamento invalido
+			move $a0, $s3				# Move o inteiro para o parametro de checkValidApartment
+			jal  checkValidApartment		# Checa se o apartamento e valido
+			move $t1, $v0				# Armazena o valor retornado em $t1
+			beqz $t1, errorInvalidApartment		# Se o valor retornado for 0, indica erro de apartamento invalido
+			
+			# Calcula o offset do endereco do apartamento
+			move $a0, $s3			# Usa o numero do apartamento como parametro pra calcular o endereco do apartamento
+			jal  calculateApartAddress	# Calcula o endereco
+			move $t1, $v0			# Armazena o offset em $t1
+			
+			move $a0, $t1
+			li $v0, 1
+			syscall
+			
+			#  Obtem a quantidade de moradores no apartamento especificado
+			la   $t2, moradores	# Armazena o endereco da quantidade de moradores em $t2
+			add  $t2, $t2, $t1	# Incrementa o endereco a quantidade de bytes que o offset indica
+			lw   $t3, 0($t2)	# Recupera a quantidade de moradores do local
+			
+			# Erro de apartamento cheio 
+			bge  $t3, 6, errorMoradores	# Se o apartamento contiver mais que 6 pessoas, erro de apartamento cheio
+			addi $t3, $t3, 1		# Se nao tiver acontecido erro, adiciona mais um morador no apartamento
+			sw   $t3, 0($t2)		# Armazena a quantidade de moradores 
+			
+			b restart
+			
+		# Bloco de rmvMorador
+		rmvMorador:
+			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
+			jal  getOptions		# Obtem as options
+			move $s2, $v0		# Armazena o endereco das options em $s2
+			
+			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions			# Conta a quantidade de options
+			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			bne  $t0, 2, errorInvalidOptions	# Se houver mais que duas options, imprime erro de Options
+			
+			b restart
+			
+		# Bloco de addAuto
+		addAuto:
+			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
+			jal  getOptions		# Obtem as options
+			move $s2, $v0		# Armazena o endereco das options em $s2
+			
+			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions			# Conta a quantidade de options
+			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			bne  $t0, 4, errorInvalidOptions	# Se houver mais que quatro options, imprime erro de Options
+			
+			b restart
+			
+		# Bloco de rmvAuto
+		rmvAuto:
+			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
+			jal  getOptions		# Obtem as options
+			move $s2, $v0		# Armazena o endereco das options em $s2
+			
+			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions			# Conta a quantidade de options
+			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			bne  $t0, 2, errorInvalidOptions	# Se houver mais que duas options, imprime erro de Options
+			
+			b restart
+			
+		# Bloco de limparAp
+		limparAp:
+			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
+			jal  getOptions		# Obtem as options
+			move $s2, $v0		# Armazena o endereco das options em $s2
+			
+			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions			# Conta a quantidade de options
+			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			bne  $t0, 1, errorInvalidOptions	# Se houver mais que duas options, imprime erro de Options
+			
+			b restart
+			
+		# Bloco de infoAp
+		infoAp:
+			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
+			jal  getOptions		# Obtem as options
+			move $s2, $v0		# Armazena o endereco das options em $s2
+			
+			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions			# Conta a quantidade de options
+			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			bne  $t0, 1, errorInvalidOptions	# Se houver mais que duas options, imprime erro de Options
+			
+			b restart
+			
+		# Bloco de infoGeral
+		infoGeral:
+			la   $s0, moradores	# Carrega o endereco de moradores em $t1
+			li   $s1, 0		# Inicializa o contador de APs
+			li   $s2, 0		# Inicializa acumulador de nao vazios
+			
+			loopAp:
+				beq  $s1, 24, endLoopAp		# Se o contador for igual a 24 (Numero de apartamentos) encerra
+				lw   $t0, 0($s0)		# Carrega a quantidade de moradores
+				
+				move $a0, $t0
+				li $v0, 1
+				syscall
+				
+				bgt  $t0, $zero, addNaoVazio	# Se a quantidade de moradores for maior que zero, adiciona apartamento nao vazio
+				ble  $t0, $zero, increment	# Se for vazio, passa para o proximo apartamento
+				
+				addNaoVazio:
+					addi $s2, $s2, 1	# Incrementa o numero de aps nao vazios
+				
+				increment:
+					addi $s1, $s1, 1		# incrementa o contador
+					addi $s0, $s0, 4		# Incrementa o endereco de moradores
+					b loopAp
+								
+			endLoopAp:				
+				la $a0, str_naoVazios
+				li $v0, 4
+				syscall
+				
+				move $a0, $s6
+				li $v0, 1
+				syscall
+			
+			b restart
+			
+		# Bloco de salvar
+		salvar:
+			li $v0, 4
+			la $a0, cmd_8
+			syscall
+			
+			b restart
+			
+		# Bloco de recarregar
+		recarregar:
+			li $v0, 4
+			la $a0, cmd_9
+			syscall
+			
+			b restart
+			
+		# Bloco de formatar o arquivo
+		formatar:
+			li $v0, 4
+			la $a0, cmd_10
+			syscall
+			
+			b restart
+			
+		# Finaliza o programa
+		finalizar:
+			b end_exec
+		
+		# Erro de comando invalido
+		error:
+			la  $a0, invalid_cmd
+			jal mmio_printString
+			b   restart
+		
+		# Erro de apartamento invalido	
+		errorInvalidApartment:
+			la  $a0, invalid_apart
+			jal mmio_printString
+			b   restart
+			
+		# Erro de apartamento cheio
+		errorMoradores:
+			la $a0, full_apart
+			jal mmio_printString
+			b restart
+		
+		# Erro de opcoes invalidas
+		errorInvalidOptions:
+			la  $a0, invalidOptions
+			jal mmio_printString
+		
+		# Reinicia o programa
+		restart:
+			jal clearOptions
+			b exec
+			
+	end_exec:
+		jal exit
 
 
-# Subprograma:	comparaTamanho
-# Proposito:	Comparar os tamanhos de duas strings
-# Input:	$a0 - endereco da string 1
-#		$a1 - endereco da string 2
-# Retorno:	$v0 - valor de comparacao (-1 se str1 for menor, 1 se st1 for maior, 0 se o tamanho for igual)
-# Side effects:	Nao se aplica
-.text
-comparaTamanho:
-	addi $sp, $sp, -16 	# Aloca 16 bytes na pilha
-	sw   $ra, 0($sp)	# Armazena o endereco de retorno na pilha
-	sw   $a1, 4($sp)	# Armazena o endereco da string 2 na pilha
-	sw   $s0, 8($sp)	# Armazena $s0 para uso neste subprograma
-	sw   $s1, 12($sp)	# Armazena $s1 para uso neste subprograma
-	
-	jal  strlen		# Obtem o tamanho da primeira string
-	move $s0, $v0		# Armazena o tamanho em $s0
-	
-	lw   $a0, 4($sp)	# Recupera a string 2 e a carrega em $a0
-	jal  strlen		# Obtem o tamanho da segunda string
-	move $s1, $v0		# Armazena o resultado em $s1
-	
-	sgt  $t0, $s0, $s1	# Verifica se str1 e maior que str2	
-	slt  $t1, $s0, $s1	# Verifica se str1 e menor que str2
-	
-	beq  $t0, 1, str1_greater 	# Branch para str1_greater se str1 for maior
-	beq  $t1, 1, str1_lesser	# Branch para str1_lesser se str1 for menor
-	
-	b    equal_strs		# ultimo caso possivel, branch equal se nenhuma das duas condicoes anteriores for satisfeita
-	
-	str1_greater:
-		addi $v0, $zero, 1		# Se str1 for maior, retorna 1
-		b end_compara_tamanho	# Branch end
-	str1_lesser:
-		addi $v0, $zero, -1		# Se str1 for menor, retorna -1
-		b end_compara_tamanho	# Branch end
-	equal_strs:
-		addi $v0, $zero, 0	# Se forem iguais, retorna 0
-	end_compara_tamanho:
-		lw   $ra, 0($sp)	# Recupera o valor de retorno da funcao
-		lw   $s0, 8($sp)	# Recupera o valor de $s0
-		lw   $s1, 12($sp)	# Recupera o valor de $s1
-		addi $sp, $sp, 16	# Devolve os 12 bytes fornecidos pela pilha
-	
-		jr   $ra		# Retorna ao programa que o chamou
+# Armazena os dados dos apartamentos
+.data
+	moradores:	 .space 96		# Array para armazenar a quantidade de moradores (24 apartamentos, armazenando um inteiro (4 bytes))
+	nomes_moradores: .space 3600		# Array que contÃ©m os nomes dos moradores de um apartamento (24 * 6 * 25)
 
 
-# Subprograma: 	removeNewline
-# Proposito: 	Remover o caractere de nova linha de uma string, se presente
-# Input: 	$a0 - endereco da string
-# Output:	$v0 - endereco da string apos a remocao
-.text
-removeNewline:
-	move $t0, $a0	# Armazena o endereco da string em $t0
+.include "mmio_utils.asm"
+.include "static.asm"
+.include "utils/utils.asm"
 
-    loop_remove:
-        lb   $t1, 0($t0)      		# Carrega o caractere atual da string
-        beq  $t1, $zero, end_remove 	# Se encontrar o final da string (NULL), terminar
-        beq  $t1, 10, remove_char 	# Se encontrar o caractere de nova linha (ASCII 10), remover
-        addi $t0, $t0, 1    		# Aponta para o proximo caractere da string
-        b loop_remove       		# Repete o loop ate encontrar o NULL
-        
-    remove_char:
-        sb $zero, 0($t0)    # Substitui o caractere de nova linha por NULL
-    
-    end_remove:
-    	move $v0, $a0
-        jr $ra              # Retorna para a funcao chamadora
-        
-        
-# Subprograma:	strcmp
-# Propï¿½sito:	Comparar duas strings
-# Input:	$a0 - endereco da string 1
-#		$a1 - endereco da string 2
-# Retorno:	$v0 - valor de comparacao:
-#			-1, se o caractere que as diferencia for menor em str1
-#			1, se o caractere que as diferencia for maior em str1
-#			0, se as strings forem iguais
-# Side effects:	Nao se aplica
-.text
-strcmp:
-	addi $sp, $sp, -16 	# Aloca 16 bytes na pilha para armazenar as variaveis a seguir:
-	sw   $ra, 0($sp)	# Armazena o valor de retorno da funcao
-	sw   $a0, 4($sp)	# Armazena o endereco da primeira string
-	sw   $a1, 8($sp)	# Armazena o endereco da segunda string
-	sw   $s0, 12($sp)	# Armazena o valor de $s0, para uso do registrador neste programa
-	
-	jal comparaTamanho	# Compara o tamanho das duas strings
-	move $t0, $v0		# Salva o valor retornado por comparaTamanho, para verificar se inicia o loop
-	
-	bnez $t0, end_strcmp	# Se o valor retornado por comparaTamanho nao for 0, jump para o final do subprograma
-	
-	lw   $a0, 4($sp)	# Endereco base da primeira string
-	lw   $a1, 8($sp)	# Endereco base da segunda string
-	move $t2, $a0		# Endereco da primeira string, para manipulacao
-	move $t3, $a1		# Endereco da segunda string, para manipulacao
-	
-	loop:
-		lb   $t0, 0($t2)	# Copia um caractere de str1
-		lb   $t1, 0($t3) 	# Copia um caractere de str2
-		
-		and  $t4, $t0, $t1	# Se um dos caracteres for '\0', jump para end_strcmp
-		beqz $t4, end_strcmp 
-		
-		sgt  $t4, $t0, $t1	# Retorna 1 se o caractere de str1 e maior que o caractere de str2			
-		beq  $t4, 1, greater	# Se o caractere de str1 for maior, jump para greater
-		slt  $t4, $t0, $t1	# Retorna 1 se o caractere de str1 e menor que o caractere de str2		
-		beq  $t4, 1, lower	# Se o caractere de str1 for menor, jump para lower
-		
-		addi $t2, $t2, 1	# Incrementa o index da string 1
-		addi $t3, $t3, 1	# Incrementa o index da string 2
-		addi $v0, $zero, 0	# Seta o valor de $v0 como 0, indicando que ate o momento, sao strings iguais
-		
-		b loop			# Recomeca o loop
-		
-	lower:
-		addi $v0, $zero, -1	# Define o valor de retorno como -1, pois str1 tem o caractere menor
-		b end_strcmp		# Jump para o fim da funcao
-	greater:
-		addi $v0, $zero, 1	# Define o valor de retorno como 1, pois str1 tem o caractere maior
-		b end_strcmp		# Jump para o fim da funcao
-	end_strcmp:
-		lw   $ra, 0($sp)	# Recupera o valor de retorno da funcao
-		lw   $s0, 12($sp)	# Recuperea o valor guardado em $s0
-		addi $sp, $sp, 16	# Devolve os 16 bytes alocados a pilha
-		
-		jr   $ra		# Retorna a funcao que chamou
-
-
-# Subprograma:		exit
-# Proposito:		Finalizar o programa
-# Input:		Nao se aplica
-# Retorno:		Nao se aplica
-# Side effects:		Encerra o programa
-.text
-exit:
-	ori  $v0, $zero, 10	# Servico 10 indica encerramento do programa
-	syscall
-	
