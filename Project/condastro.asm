3f12fc094802b20c1044c496905f3af6c5875841
Higor Cordeiro <higormatheus1618@gmail.com>
2024-08-09 16:12:56 -0300

    Finalizado addMorador e infoGeral. Testes foram realizados e se demonstraram funcionais. Próximo passo: criar infoAp

--- Project/condastro.asm
+++ Project/condastro.asm
@@ -9,6 +9,7 @@ main:
 		jal removeNewline	# Remove o caractere de newLine
 		move $s1, $v0		# Armazena o valor em $s1
 	
+		# Obtem o comando
 		move $a0, $s1		# Armazena o input em $a0, para ser usado como parametro de getCommand
 		jal  getCommand		# Obtem o comando apos o input
 		move $a0, $v0		# Armazena o comando em $s0
@@ -17,86 +18,86 @@ main:
 		
 		
 		# Case addMorador:
-		move $a0, $s0		# Passa a string como parametro para strcmp		
-		la   $a1, cmd_1		# Passa o comando 1 como parametro para strcmp
-		jal  strcmp			# Compara as strings
-		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		move $a0, $s0			# Passa a string como parametro para strcmp		
+		la   $a1, cmd_1			# Passa o comando 1 como parametro para strcmp
+		jal  strcmp				# Compara as strings
+		move $t0, $v0			# Se forem iguais, $v0 retorna 0
 		beqz $t0, addMorador	# Se v0 retornar 0, jump para addMorador
 		
 		# Case rmvMorador:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_2		# Passa o comando 2 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, rmvMorador	# Se v0 retornar 0, jump para rmvMorador
 		
 		# Case addAuto:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_3		# Passa o comando 3 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, addAuto	# Se v0 retornar 0, jump para addAuto
 		
 		# Case rmvAuto
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_4		# Passa o comando 4 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, rmvAuto	# Se v0 retornar 0, jump para rmvAuto
 		
 		# Case limparAp
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_5 	# Passa o comando 5 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, limparAp	# Se v0 retornar 0, jump para limparAp
 		
 		# Case infoAp:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_6		# Passa o comando 6 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, infoAp	# Se v0 retornar 0, jump para infoAp
 		
 		# Case infoGeral:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_7		# Passa o comando 7 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, infoGeral	# Se v0 retornar 0, jump para infoGeral
 		
 		# Case salvar:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_8		# Passa o comando 8 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, salvar	# Se v0 retornar 0, jump para salvar
 		
 		# Case recarregar:
-		move $a0, $s0		# Passa a string como parametro para strcmp		
-		la   $a1, cmd_9		# Passa o comando 9 como parametro para strcmp
-		jal  strcmp		# Compara as strings
-		move $t0, $v0		# Se forem iguais, $v0 retorna 0
+		move $a0, $s0			# Passa a string como parametro para strcmp		
+		la   $a1, cmd_9			# Passa o comando 9 como parametro para strcmp
+		jal  strcmp				# Compara as strings
+		move $t0, $v0			# Se forem iguais, $v0 retorna 0
 		beqz $t0, recarregar	# Se v0 retornar 0, jump para recarregar
 		
 		# Case formatar:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_10	# Passa o comando 10 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, formatar	# Se v0 retornar 0, jump para formatar
 		
 		# Case finalizar:
 		move $a0, $s0		# Passa a string como parametro para strcmp		
 		la   $a1, cmd_11	# Passa o comando 11 como parametro para strcmp
-		jal  strcmp		# Compara as strings
+		jal  strcmp			# Compara as strings
 		move $t0, $v0		# Se forem iguais, $v0 retorna 0
 		beqz $t0, finalizar	# Se v0 retornar 0, jump para finalizar
 		
 		# Default:
 		b error			# Branch para Comando Invalido
 		
-		# Bloco de addMorador
+		# Bloco de addMorador (Finalizado)
 		addMorador:
 			# Obtem as options
 			move $a0, $s1		# Utiliza a string de comando como parametro para getOptions
@@ -104,44 +105,62 @@ main:
 			move $s2, $v0		# Armazena o endereco das options em $s2
 			
 			# Erro de opcoes invalidas
-			move $a0, $s2				# Utiliza o endereco das options como parametro para countOptions
-			jal  countOptions			# Conta a quantidade de options
-			move $t0, $v0				# Armazena em $t0 a quantidade de options
+			move $a0, $s2						# Utiliza o endereco das options como parametro para countOptions
+			jal  countOptions					# Conta a quantidade de options
+			move $t0, $v0						# Armazena em $t0 a quantidade de options
 			bne  $t0, 2, errorInvalidOptions	# Se houver mais que duas options, imprime erro de Options
 			
 			# Converte strings numericas para inteiros
-			move $a0, $s2				# Passa $s2 como parametro para optionToInt
-			jal  optionToInt			# Transforma o valor contido na primeira option em inteiro
-			move $s3, $v0				# Armazena o retorno em $s3
-			beqz $s3, errorInvalidOptions		# Se o retorno for igual a 0, houve erro de caracteres
+			move $a0, $s2					# Passa $s2 como parametro para optionToInt
+			jal  optionToInt				# Transforma o valor contido na primeira option em inteiro
+			move $s3, $v0					# Armazena o retorno em $s3
+			beqz $s3, errorInvalidOptions	# Se o retorno for igual a 0, houve erro de caracteres
 			
 			# Erro de apartamento invalido
-			move $a0, $s3				# Move o inteiro para o parametro de checkValidApartment
-			jal  checkValidApartment		# Checa se o apartamento e valido
-			move $t1, $v0				# Armazena o valor retornado em $t1
+			move $a0, $s3						# Move o inteiro para o parametro de checkValidApartment
+			jal  checkValidApartment			# Checa se o apartamento e valido
+			move $t1, $v0						# Armazena o valor retornado em $t1
 			beqz $t1, errorInvalidApartment		# Se o valor retornado for 0, indica erro de apartamento invalido
 			
 			# Calcula o offset do endereco do apartamento
-			move $a0, $s3			# Usa o numero do apartamento como parametro pra calcular o endereco do apartamento
+			move $a0, $s3				# Usa o numero do apartamento como parametro pra calcular o endereco do apartamento
 			jal  calculateApartAddress	# Calcula o endereco
-			move $t1, $v0			# Armazena o offset em $t1
+			move $t1, $v0				# Armazena o offset em $t1
 			
 			#  Obtem a quantidade de moradores no apartamento especificado
-			la   $t2, moradores	# Armazena o endereco da quantidade de moradores em $t2
-			add  $t2, $t2, $t1	# Incrementa o endereco a quantidade de bytes que o offset indica
-			lw   $t3, 0($t2)	# Recupera a quantidade de moradores do local
+			la   $t2, moradores		# Armazena o endereco da quantidade de moradores em $t2
+			add  $t2, $t2, $t1		# Incrementa o endereco a quantidade de bytes que o offset indica
+			lw   $t3, 0($t2)		# Recupera a quantidade de moradores do local
 			
 			# Erro de apartamento cheio 
 			bge  $t3, 6, errorMoradores	# Se o apartamento contiver mais que 6 pessoas, erro de apartamento cheio
-			addi $t3, $t3, 1		# Se nao tiver acontecido erro, adiciona mais um morador no apartamento
-			sw   $t3, 0($t2)		# Armazena a quantidade de moradores 
+			addi $t3, $t3, 1			# Se nao tiver acontecido erro, adiciona mais um morador no apartamento
+			sw   $t3, 0($t2)			# Armazena a quantidade de moradores 
 			
 			# Adicao do nome do morador no endereco correto
-			move $a0, $s3
-			jal calculateNomeAddress
-			move $a0, $v0
-			li $v0, 1
-			syscall
+			move $a0, $s3				# Usa o numero do apartamento como parametro de calculateNomeAddress
+			jal  calculateNomeAddress	# Calcula o endereco do apartamento para armazenamento dos nomes
+			la   $t0, nomes_moradores	# Armazena o endereco de nomes_moradores em $t0
+			add  $t0, $t0, $v0			# Move ate o index retornado por calculateNomeAddress
+			
+			add  $s2, $s2, 25			# Incrementa o endereco de options (option 2)
+			
+			# Loop para encontrar o espaco disponivel em nomes_moradores
+			loopFindAvailable:
+				lb   $t1, 0($t0)			# Carrega o primeiro caractere do endereco
+				bnez $t1, incrementIndex	# Se o caractere nao for nulo, incrementa o index
+				b    store					# Se for um caractere nulo, armazena o nome
+				
+			# Incrementa o index de nomes_moradores 
+			incrementIndex:	
+				addi $t0, $t0, 25			# Incrementa em 25 bytes o index (proximo espaco de nome)
+				b    loopFindAvailable		# Recomeca o loop de procura
+				
+			# Armazena o nome inserido
+			store:
+				move $a0, $t0	# Usa nomes_moradores como destino de copia
+				move $a1, $s2	# Usa a option 2 como origem de copia
+				jal  strcpy		# Copia option 2 em nomes_moradores
 			
 			b restart
 			
@@ -210,7 +229,7 @@ main:
 			
 			b restart
 			
-		# Bloco de infoGeral
+		# Bloco de infoGeral (Finalizado)
 		infoGeral:
 			la   $s0, moradores	# Carrega o endereco de moradores em $t1
 			li   $s1, 0		# Inicializa o contador de APs
@@ -227,21 +246,35 @@ main:
 					addi $s2, $s2, 1	# Incrementa o numero de aps nao vazios
 				
 				increment:
-					addi $s1, $s1, 1		# incrementa o contador
-					addi $s0, $s0, 4		# Incrementa o endereco de moradores
+					addi $s1, $s1, 1	# incrementa o contador
+					addi $s0, $s0, 4	# Incrementa o endereco de moradores
 					b    loopAp			# Recomeca o loop
 								
-			endLoopAp:				
+			endLoopAp:			
+				# Imprime a quantidade de nao vazios	
 				la  $a0, str_naoVazios	# Recebe o endereco de str_naoVazios
 				jal mmio_printString	# Imprime no mmio
-				
-				move $a0, $s2		# Usa a quantidade de nao vazios para impressao
-				jal  intToStr		# Converte o inteiro para string
-				move $a0, $v0		# Usa a string obtida para impressao
+				move $a0, $s2			# Usa a quantidade de nao vazios para impressao
+				jal  intToStr			# Converte o inteiro para string
+				move $a0, $v0			# Usa a string obtida para impressao
 				jal  mmio_printString	# Imprime a string
-				la   $a0, newLine	# Usa newLine como parametro de printString
+				la   $a0, newLine		# Usa newLine como parametro de printString
 				jal  mmio_printString	# Imprime newLine
-			
+				
+				# Apartamentos vazios
+				li   $s3, 24			# Armazena a quantidade total de apartamentos
+				sub  $s3, $s3, $s2		# Obtem apartamentos vazios por meio de apartamentos - apartamentos nao vazios
+				
+				# Imprime a quantidade de vazios
+				la   $a0, str_vazios	# usa str_vazios como parametro para printString
+				jal  mmio_printString	# Imprime str_vazios
+				move $a0, $s3			# Usa a quantidade de vazios como parametro de intToStr
+				jal  intToStr			# Converte para string
+				move $a0, $v0			# Usa a string resultante como parametro de printString
+				jal  mmio_printString	# Imprime a quantidade de vazios
+				la   $a0, newLine		# Usa newLine como parametro de printString
+				jal  mmio_printString	# Imprime newLine
+				
 			b restart
 			
 		# Bloco de salvar
--- Project/mmio_utils.asm
+++ Project/mmio_utils.asm
@@ -533,3 +533,38 @@ calculateNomeAddress:
 	jr   $ra	# Retorna para a funcao que o chamou
 	
 	
+# Subprograma:		strcpy
+# Propósito:		Copiar a string contida na origem para o endereço destino
+# Input:		$a0 - endereço de memória destino
+#			$a1 - endereço de memória origem
+# Retorno:		$v0 - endereço de memória destino
+.text
+strcpy:
+	addi $sp, $sp, -8	# Alocando 8 bytes na pilha para armazenar endereço de retorno e $s0
+	sw   $ra, 0($sp)	# Armazenando o valor de retorno na pilha
+	sw   $s0, 4($sp)	# Armazenando $s0 para disponibilizá-lo no subprograma
+	
+	move $s0, $a0	# Copiando o endereço de destino no registrador temporário $t1
+	move $t1, $a1	# Copiando o endereço de origem no registrador $t1
+	move $s1, $a0	# Mantendo o endereço base da string de destino
+	
+	loop_strcpy:
+		lb   $t0, 0($t1)	# Copia o primeiro caractere da origem em $t0
+		sne  $t3, $t0, $zero	# Compara o caractere com '\0' e guarda o booleano em $t3
+		beqz $t3, end_strcpy  	# Se $t3 for igual a 0, branch para end_strcpy
+			sb   $t0, 0($s0)	# Armazena o caractere encontrado na string destino
+			addi $t1, $t1, 1	# Segue para o próximo caractere da origem
+			addi $s0, $s0, 1	# Segue para o próximo caractere do destino
+			
+		b loop_strcpy			# Recomeça o loop
+		
+	end_strcpy:
+		addi $s0, $s0, 1	# Segue para o próximo espaço livre na memória
+		sb   $zero, 0($s0)	# Adiciona o caractere nulo no final da string de destino
+	
+		move $v0, $s1		# Copia o endereço base de destino no endereço de retorno da função
+		lw   $ra, 0($sp)	# Recupera o valor de retorno da função
+		lw   $s0, 4($sp)	# Recupera o valor de $s0
+		addi $sp, $sp, 8	# Devolve a memória à pilha
+		
+		jr $ra			# Retorna à função de origem
